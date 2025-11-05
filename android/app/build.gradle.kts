342f69b0fd6d7738f19ab5d40946d3971ed79e49
adl <makara3416@gmail.com>
2025-11-05 13:46:50 +0100

    v

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.turkradyo.adl.de.turkradyo"
+    namespace = "com.turkradyo.bsr.de.turkradyo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -30,10 +30,10 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.turkradyo.adl.de.turkradyo"
+    applicationId = "com.turkradyo.bsr.de.turkradyo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21  // Android 5.0 ve üzeri - API Level 21 (Samsung S9 uyumluluğu için)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
---
+++ android/app/src/main/kotlin/com/turkradyo/bsr/de/turkradyo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.turkradyo.bsr.de.turkradyo
+
+import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
+
+class MainActivity : AudioServiceActivity()
